abf8167fdf013cb29324625f2adf69ec4e3115f5
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2024-03-28 10:35:56 +1000

    this is not the kind of commit message you should write. Its uninformative and rather useless

--- src/csv_to_parquet.py
+++ src/csv_to_parquet.py
@@ -1,5 +1,5 @@
 """
-
+This doesn't work yet!!!
 """
 import pandas as pd
 import os
@@ -11,7 +11,10 @@ class DataProcessor:
 
     def load_csv(self, relative_path, parse_dates=None):
         """Load a CSV file into a DataFrame."""
-        file_path = os.path.join(self.base_path, relative_path)
+        file_path = os.path.join(
+            self.base_path,
+            relative_path
+        )
         return pd.read_csv(file_path, parse_dates=parse_dates)
 
     def save_parquet(self, df, relative_path):
@@ -19,9 +22,10 @@ class DataProcessor:
         file_path = os.path.join(self.base_path, relative_path)
         df.to_parquet(file_path)
 
+
 class CFG:
     data_path = './../data'
-    file_name = 'totaldemand_nsw.csv'
+    file_name = 'NSW_consolidated.csv'
     date_fields = ['DATETIME']
     img_dim1 = 20
     img_dim2 = 10
@@ -32,16 +36,16 @@ if __name__ == "__main__":
     processor = DataProcessor(CFG.data_path)
 
     # Load the dataset
-    totaldemand_nsw = processor.load_csv(os.path.join(CFG.data_path, 'NSW', CFG.file_name),
-		parse_dates=CFG.date_fields
-	)
+    totaldemand_nsw = processor.load_csv(
+        os.path.join(CFG.data_path, 'NSW', CFG.file_name),
+        parse_dates=CFG.date_fields
+    )
 
     # Inspect the loaded data
     print(totaldemand_nsw.head())
     print(totaldemand_nsw.dtypes)
 
     # Save the dataset in Parquet format
-    processor.save_parquet(
-        totaldemand_nsw,
-        'NSW/totaldemand_nsw.parquet'
-    )
+    processor.save_parquet(relative_path=os.path.join(
+        CFG.data_path,
+        'NSW_consolidated.parquet'))
