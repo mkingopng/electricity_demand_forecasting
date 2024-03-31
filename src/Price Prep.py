e199071c288d6b97df80df859fbe7800bce52ee5
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2024-03-31 17:58:01 +1000

    gotta forgive me Noel :) small changes to make it pep-8 compliant

--- src/Price Prep.py
+++ src/Price Prep.py
@@ -4,8 +4,9 @@ import matplotlib.pyplot as plt
 
 data_path = './../Noels_folder'
 
-def GetFileList(data_path):
-    print(' ******************** : Scan Folder for Files : ********************')
+
+def get_file_list(data_path):
+    print('******************** : Scan Folder for Files : *******************')
     price_data_folder = os.path.join(data_path,'P_Folder')
     files_to_load = []
 
@@ -16,7 +17,8 @@ def GetFileList(data_path):
 
     return files_to_load
 
-def CombineFilesToDF(files_to_load):
+
+def combine_files_to_df(files_to_load):
     print(' ******************** : Load & Combine Data: ********************')
     dfs = []
     loaded =0
@@ -34,18 +36,19 @@ def CombineFilesToDF(files_to_load):
     print(combined_df.dtypes)
     return combined_df
 
-def DataPreparation(combined_df):
+
+def data_preparation(combined_df):
     print(' ******************** : Data Preparation: ********************')
-    #DF with relevant columns
+    # DF with relevant columns
     combined_df = combined_df[['SETTLEMENTDATE','TOTALDEMAND','RRP']]
-    price_df = combined_df.rename(columns={'SETTLEMENTDATE':'date','TOTALDEMAND':'totaldemand','RRP':'rrp'})
+    price_df = combined_df.rename(columns={'SETTLEMENTDATE': 'date', 'TOTALDEMAND': 'totaldemand', 'RRP': 'rrp'})
     print(price_df.columns)
 
-    #Convert Date
+    # Convert Date
     price_df['date'] = pd.to_datetime(price_df['date'])
     print(price_df['date'].dtypes)
 
-    #Aggregate Data to 30 minute intervals
+    # Aggregate Data to 30 minute intervals
     price_df.set_index('date',inplace=True)
     price_df_clean = price_df.resample('30min').mean().reset_index()
     # price_df_clean.reset_index()
@@ -54,24 +57,25 @@ def DataPreparation(combined_df):
     print(len(price_df_clean))
     print(price_df_clean)
 
-    #Outliers Removed
+    # Outliers Removed
     price_upper = 2000
     price_lower = -650
 
-    #Price Outliers
+    # Price Outliers
     price_outliers_upper = price_df_clean[(price_df_clean['rrp'] > price_upper)]
     price_outliers_lower = price_df_clean[(price_df_clean['rrp'] < price_lower)]
-    price_outliers = pd.concat([price_outliers_upper,price_outliers_lower],axis=0)
+    price_outliers = pd.concat([price_outliers_upper, price_outliers_lower], axis=0)
     print(f'Total Rows: {len(price_outliers)}')
     # print(price_outliers.describe)
 
-    price_outlier_removed_df = price_df_clean[(price_df_clean['rrp'] < price_upper) & (price_df_clean['rrp']>price_lower)]
+    price_outlier_removed_df = price_df_clean[(price_df_clean['rrp'] < price_upper) & (price_df_clean['rrp'] > price_lower)]
     print(f'Total Rows: {len(price_outlier_removed_df)}')
     # print(price_outlier_removed_df.describe)
 
     return price_df_clean, price_outliers, price_outlier_removed_df
 
-def DataQualityChecks(price_df_clean, price_outliers, price_outlier_removed_df):
+
+def data_quality_checks(price_df_clean, price_outliers, price_outlier_removed_df):
     print(' ******************** : Data Quality: ********************')
 
     print(f'Price Data (Clean): {price_df_clean.shape}')
@@ -84,7 +88,8 @@ def DataQualityChecks(price_df_clean, price_outliers, price_outlier_removed_df):
     print(price_outliers.isna().sum())
     print(price_outliers.nunique())
 
-def DataVisualisation(price_df_clean, price_outliers, price_outlier_removed_df):
+
+def data_visualisation(price_df_clean, price_outliers, price_outlier_removed_df):
     print(' ******************** : Data Visualisation: ********************')
 
     plt.scatter(price_df_clean['date'],price_df_clean['rrp'])
@@ -92,10 +97,10 @@ def DataVisualisation(price_df_clean, price_outliers, price_outlier_removed_df):
     plt.show()
 
 
-if __name__=='__main__':
-   files_to_load =  GetFileList(data_path)
-   combined_df = CombineFilesToDF(files_to_load)
-   price_df_clean, price_outliers, price_outlier_removed_df = DataPreparation(combined_df)
-   DataQualityChecks(price_df_clean,price_outlier_removed_df, price_outliers)
-   DataVisualisation(price_df_clean, price_outliers, price_outlier_removed_df)
+if __name__ == '__main__':
+   files_to_load = get_file_list(data_path)
+   combined_df = combine_files_to_df(files_to_load)
+   price_df_clean, price_outliers, price_outlier_removed_df = data_preparation(combined_df)
+   data_quality_checks(price_df_clean, price_outlier_removed_df, price_outliers)
+   data_visualisation(price_df_clean, price_outliers, price_outlier_removed_df)
    # DataVisualisation(price_df_clean, price_outliers, price_outlier_removed_df)
