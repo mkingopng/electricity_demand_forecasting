d31f532205bd2ebca7ba0c02087c09470131afa8
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2024-04-05 13:38:19 +1000

    2 mothods for tuning hypers now

--- src/xgb_tuning.py
+++ src/xgb_tuning.py
@@ -1,58 +1,19 @@
 """
 
 """
-import xgboost as xgb
-import optuna
-import pandas as pd
-from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
+from xgb_functions import *
+import gc
 
 n_in = 6  # 6 lag intervals
 n_test = 336  # 7 days of 30-minute sample intervals
 
-def series_to_supervised(data, n_in=1, n_out=1, dropnan=True, target_var='TOTALDEMAND'):
-    """
-    frame a time series dataset as a supervised learning dataset, required as
-    in input for xgb
-    :param data:
-    :param n_in:
-    :param n_out:
-    :param dropnan:
-    :param target_var:
-    :return:
-    """
-    n_vars = 1 if type(data) is list else data.shape[1]
-    df = pd.DataFrame(data)
-    cols, names = list(), list()
-
-    for i in range(n_in, 0, -1):  # input sequence (t-n, ... t-1)
-        cols.append(df.drop(columns=target_var).shift(i))
-        names += [('%s(t-%d)' % (df.columns[j], i)) for j in range(n_vars) if
-                  df.columns[j] != target_var]
-
-    # forecast sequence (t+1, ... t+n_out-1), only for predictors, not target
-    for i in range(1, n_out):
-        cols.append(df.drop(columns=target_var).shift(-i))
-        names += [('%s(t+%d)' % (df.columns[j], i)) for j in range(n_vars) if
-                  df.columns[j] != target_var]
-
-    # add the target variable column at t (current timestep)
-    cols.append(df[[target_var]])
-    names.append('%s(t)' % target_var)
-
-    # combine everything
-    agg = pd.concat(cols, axis=1)
-    agg.columns = names
-
-    # drop records with NaN values
-    if dropnan:
-        agg.dropna(inplace=True)
-
-    return agg
-
 
 def objective(trial):
-    # define the hyperparameters to be tuned
+    """
+    define the hyperparameters to be tuned
+    :param trial:
+    :return:
+    """
     params = {
         'objective': 'reg:squarederror',
         'gamma': trial.suggest_float('gamma', 0.1, 6.0),
@@ -94,7 +55,7 @@ def objective(trial):
     # predict on validation data
     y_pred = bst.predict(dval)
     error = mean_absolute_error(y_val, y_pred)
-
+    gc.collect()
     # return the metric you want to minimize
     return error
 
